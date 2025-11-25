2b76b9538d46b821d31e7664d3faaad0631169e1
Rémi Dubois <remi.dubois@gmail.com>
2025-11-25 15:20:48 +0100

    Back to development: 1.4.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = f"""
 
 setup(
     name="pas.plugins.kimug",
-    version="1.4.1",
+    version="1.4.2.dev0",
     description="A PAS plugin to set roles to imio keycloak users",
     long_description=long_description,
     long_description_content_type="text/markdown",
